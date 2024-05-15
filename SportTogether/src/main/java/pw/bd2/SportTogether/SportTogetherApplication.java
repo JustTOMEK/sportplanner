a4c0744d4250dc61b26b8525c4d3de5b7de773a6
Piotr Lenczewski <piotrlenczewskipl@o2.pl>
2024-05-15 13:56:56 +0200

    Generated spring boot project files

---
+++ SportTogether/src/main/java/pw/bd2/SportTogether/SportTogetherApplication.java
@@ -0,0 +1,13 @@
+package pw.bd2.SportTogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportTogetherApplication.class, args);
+	}
+
+}
---
+++ SportTogether/src/test/java/pw/bd2/SportTogether/SportTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package pw.bd2.SportTogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
