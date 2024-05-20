8826238e14fabbcce2e7c93e6f4812407144d665
Michał Machnikowski <01178559@pw.edu.pl>
2024-05-20 13:21:45 +0200

    Added script checking connection with database

---
+++ backend/check_db_connection.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os
+
+import mysql.connector
+
+
+credentials = {
+    "host": os.environ["DB_HOST"],
+    "port": os.environ["DB_PORT"],
+    "database": os.environ["DB_NAME"],
+    "user": os.environ["DB_USER"],
+    "password": os.environ["DB_PASSWORD"],
+}
+
+with mysql.connector.connect(**credentials) as connection, connection.cursor() as cursor:
+    cursor.execute("SELECT * FROM testtable;")
+    for row in cursor.fetchall():
+        print(row)
