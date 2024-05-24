2b608a7e49456810089e88751f20012cbf9e1994
Piotr Lenczewski <piotrlenczewskipl@o2.pl>
2024-05-24 11:08:19 +0200

    Added tests for User and UserRepository

--- backend/src/main/java/pw/bd2/SportTogether/model/User.java
+++ backend/src/main/java/pw/bd2/SportTogether/model/User.java
@@ -7,22 +7,32 @@ import lombok.*;
 @Setter
 @NoArgsConstructor
 @RequiredArgsConstructor
-@AllArgsConstructor
 @Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, updatable = false)
     private Integer id;
 
+    @NonNull
     @Column(nullable = false, length = 20)
     private String username;
 
     @Column(length = 50)
     private String email;
 
+    @NonNull
     @Column(name = "password_hash", nullable = false, length = 512)
     private String passwordHash;
 
+    @NonNull
     @Column(nullable = false, length = 30)
     private String salt;
+
+    public User(String username, String email, String passwordHash, String salt) {
+        this.username = username;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.salt = salt;
+    }
 }
