664638e0010c815db1d0ba1f6c5f1e35f7778856
Piotr Lenczewski <piotrlenczewskipl@o2.pl>
2024-05-27 09:59:20 +0200

    Way for extracting username in controller

--- backend/src/main/java/pw/bd2/SportTogether/controller/UserController.java
+++ backend/src/main/java/pw/bd2/SportTogether/controller/UserController.java
@@ -1,15 +1,21 @@
 package pw.bd2.SportTogether.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pw.bd2.SportTogether.service.JwtService;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/users")
 public class UserController {
+    private final JwtService jwtService;
     @GetMapping
-    public ResponseEntity<String> sayHello() {
-        return ResponseEntity.ok("Hello from secured endpoint");
+    public ResponseEntity<String> sayHello(@RequestHeader(HttpHeaders.AUTHORIZATION) String jwt) {
+        return ResponseEntity.ok(jwtService.extractUsername(jwt));
     }
 }
