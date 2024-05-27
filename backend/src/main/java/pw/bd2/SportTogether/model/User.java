69773f618a034c57d58765f0af8479f9f4457f6f
Piotr Lenczewski <piotrlenczewskipl@o2.pl>
2024-05-27 09:58:51 +0200

    Changed id generation strategy

--- backend/src/main/java/pw/bd2/SportTogether/model/User.java
+++ backend/src/main/java/pw/bd2/SportTogether/model/User.java
@@ -17,7 +17,7 @@ import java.util.List;
 @Table(name = "users")
 public class User implements UserDetails {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String username;
