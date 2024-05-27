31fb4ca2f68f021daaa3b599993684fc85ed3898
Jakub Pęk <01178575@pw.edu.pl>
2024-05-27 16:00:31 +0200

    Fixed misspelled name

--- backend/src/main/java/pw/bd2/SportTogether/controller/AuthenticationController.java
+++ backend/src/main/java/pw/bd2/SportTogether/controller/AuthenticationController.java
@@ -18,14 +18,14 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> resgister(
+    public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
     ){
         return ResponseEntity.ok(authenticationService.register(request));
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate(
+    public ResponseEntity<AuthenticationResponse> register(
             @RequestBody AuthenticationRequest request
     ){
         return ResponseEntity.ok(authenticationService.authenticate(request));
