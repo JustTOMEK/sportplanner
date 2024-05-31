59bc94b36026f4f8956512ed4df0a71262a0fc32
Michał Machnikowski <01178559@pw.edu.pl>
2024-05-31 20:04:06 +0200

    Corrected table name for users in Spring

--- backend/src/main/java/pw/bd2/SportTogether/model/User.java
+++ backend/src/main/java/pw/bd2/SportTogether/model/User.java
@@ -14,7 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
