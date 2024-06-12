50d0e0e1306d314e8aa70822bde9044678e48996
Michał Machnikowski <01178559@pw.edu.pl>
2024-06-12 12:19:33 +0200

    Added example admin user

--- model/fill.sql
+++ model/fill.sql
@@ -1,3 +1,4 @@
+INSERT INTO user (username, password, role) VALUES ('admin', '$2a$10$wuQLIafDI3E37//wfrTvf.evfq0v3/AxSWGkz5Ef6MWpY4x0LXm1m', 'ADMIN'); -- password: 'admin'
 INSERT INTO user (username, password, role) VALUES ('john', '$2a$10$tLY8EPU1vZLwWG57IXt8RO6z6ZCAXtR6GigFyL.kAJlxci0XtbtA6', 'USER'); -- password: 'john'
 
 INSERT INTO sport (name) VALUES ('Other');
@@ -5,5 +6,5 @@ INSERT INTO sport (name) VALUES ('Football');
 
 INSERT INTO address (country, city) VALUES ('Poland', 'Warsaw');
 
-INSERT INTO event (title, owner_id, sport_id, address_id) VALUES ('Test event', 1, 2, 1);
+INSERT INTO event (title, owner_id, sport_id, address_id) VALUES ('Test event', 2, 2, 1);
 
