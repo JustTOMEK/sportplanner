12f6b491e2a4769f0dc4362363a2d79cf2fb45ca
Michał Machnikowski <01178559@pw.edu.pl>
2024-06-12 12:24:43 +0200

    Specified ids when filling with example just to be safe

--- model/fill.sql
+++ model/fill.sql
@@ -1,10 +1,10 @@
-INSERT INTO user (username, password, role) VALUES ('admin', '$2a$10$wuQLIafDI3E37//wfrTvf.evfq0v3/AxSWGkz5Ef6MWpY4x0LXm1m', 'ADMIN'); -- password: 'admin'
-INSERT INTO user (username, password, role) VALUES ('john', '$2a$10$tLY8EPU1vZLwWG57IXt8RO6z6ZCAXtR6GigFyL.kAJlxci0XtbtA6', 'USER'); -- password: 'john'
+INSERT INTO user (id, username, password, role) VALUES (1, 'admin', '$2a$10$wuQLIafDI3E37//wfrTvf.evfq0v3/AxSWGkz5Ef6MWpY4x0LXm1m', 'ADMIN'); -- password: 'admin'
+INSERT INTO user (id, username, password, role) VALUES (2, 'john', '$2a$10$tLY8EPU1vZLwWG57IXt8RO6z6ZCAXtR6GigFyL.kAJlxci0XtbtA6', 'USER'); -- password: 'john'
 
-INSERT INTO sport (name) VALUES ('Other');
-INSERT INTO sport (name) VALUES ('Football');
+INSERT INTO sport (id, name) VALUES (1, 'Other');
+INSERT INTO sport (id, name) VALUES (2, 'Football');
 
-INSERT INTO address (country, city) VALUES ('Poland', 'Warsaw');
+INSERT INTO address (id, country, city) VALUES (1, 'Poland', 'Warsaw');
 
-INSERT INTO event (title, description, owner_id, sport_id, address_id) VALUES ('Test event', 'Example description', 2, 2, 1);
+INSERT INTO event (id, title, description, owner_id, sport_id, address_id) VALUES (1, 'Test event', 'Example description', 2, 2, 1);
 
