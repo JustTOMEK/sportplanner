864a8fe29408bfd4f18734fb827ca0d35632b419
Michał Machnikowski <01178559@pw.edu.pl>
2024-06-12 11:41:15 +0200

    Added SQL for filling database with test data

---
+++ model/fill.sql
@@ -0,0 +1,8 @@
+INSERT INTO user (username, password, role) VALUES ('john', '$2a$10$tLY8EPU1vZLwWG57IXt8RO6z6ZCAXtR6GigFyL.kAJlxci0XtbtA6', 'USER'); -- password: 'john'
+
+INSERT INTO sport (name) VALUES ('Football');
+
+INSERT INTO address (country, city) VALUES ('Poland', 'Warsaw');
+
+INSERT INTO event (title, owner_id, sport_id, address_id) VALUES ('Test event', 1, 1, 1);
+
