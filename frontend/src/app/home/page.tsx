80eab3f281ad402a204982c5bca4524b91ec4f4f
Jakub Pęk <01178575@pw.edu.pl>
2024-06-21 20:53:20 +0200

    Added buttons to home page

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import "../../Styles/HomePage.css";
 
 const HomePage = () => {
     const router = useRouter();
@@ -19,25 +20,48 @@ const HomePage = () => {
         // setIsLoggedIn(false);
         localStorage.removeItem('token');
         alert('User logged out');
-        router.push('/auth/singin');
+        router.push('/auth/signin');
     };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-brand-secondary">
-        <h1 className="text-3xl font-bold text-brand-primary">Home page</h1>
-        <h2 className="text-lg font-bold text-brand-primary">Work in progress…</h2>
-        <br></br>
-        {hasToken ? (
-          <h1 className="text-2xl font-bold text-brand-primary">Masz token</h1>
-        ) : (
-          <h1 className="text-2xl font-bold text-brand-primary">No token found</h1>
-        )}
+        <h1 className="header">Home page</h1>
+        {/*<h2 className="text-lg font-bold text-brand-primary">Work in progress…</h2>*/}
+        {/*<br></br>*/}
+        {/*{hasToken ? (*/}
+        {/*  <h1 className="text-2xl font-bold text-brand-primary">Masz token</h1>*/}
+        {/*) : (*/}
+        {/*  <h1 className="text-2xl font-bold text-brand-primary">No token found</h1>*/}
+        {/*)}*/}
         <button
-            className="mybutton"
+            className="logout_button"
             onClick={(e) => handleLogout()}
         >
             Sign out
         </button>
+
+        <button
+            className="search_button"
+            onClick={(e) => router.push('/search')}
+        >Search events</button>
+
+        <div className="events-container">
+            <div className="events-header events-header_1">
+                <h2>Events you are attending:</h2>
+            </div>
+            <div className="events-header events-header_2">
+                <h2>Events you are hosting:</h2>
+                <button
+                    className="create_event_button"
+                    onClick={() => router.push('/event/create')}
+                >
+                    Create event
+                </button>
+            </div>
+        </div>
+
+
+
     </div>
   );
 };
