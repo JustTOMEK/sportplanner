5db3f4e71b3f6068a4e94de379f409dc3268d6d4
Jakub Pęk <01178575@pw.edu.pl>
2024-06-22 21:09:03 +0200

    Implemented simple welcome page

--- frontend/src/app/welcome/page.tsx
+++ frontend/src/app/welcome/page.tsx
@@ -1,14 +1,21 @@
 'use client';
 
-import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import withAuth from '../auth/component/withAuth';
-
+import '../../Styles/WelcomePage.css';
 
 function WelcomePage({ onLogout }) {
+    const router = useRouter();
+
+    const handleLoginClick = () => {
+        router.push('/auth/signin');
+    };
+
     return (
-        <div>
+        <div className="welcome-page">
             <h1>Welcome to the WelcomePage!</h1>
+            <p>This is a sample welcome page for our application. Here you can find some basic information about our services and features.</p>
+            <button onClick={handleLoginClick}>Login</button>
         </div>
     );
 }
