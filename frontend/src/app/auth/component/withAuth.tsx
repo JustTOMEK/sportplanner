a154a483c55a11d1b60f734b2ea31b3a684a7ecc
Tomeczek <trunio31@gmail.com>
2024-06-22 15:10:15 +0200

    Authorization

---
+++ frontend/src/app/auth/component/withAuth.tsx
@@ -0,0 +1,28 @@
+// app/hoc/withAuth.tsx
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+const withAuth = (WrappedComponent) => {
+  return (props) => {
+    const router = useRouter();
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        router.push('/auth/signin');
+      } else {
+        setLoading(false);
+      }
+    }, [router]);
+
+    if (loading) {
+      return <p>Loading...</p>; // You can replace this with a loading spinner if you have one
+    }
+
+    return <WrappedComponent {...props} />;
+  };
+};
+
+export default withAuth;
