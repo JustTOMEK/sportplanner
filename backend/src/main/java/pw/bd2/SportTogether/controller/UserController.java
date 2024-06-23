4b532a4c6bc972fa6acaf14ae920ec1592811340
Jakub Pęk <01178575@pw.edu.pl>
2024-06-23 13:02:11 +0200

    Added controller for getting info about logged user

--- backend/src/main/java/pw/bd2/SportTogether/controller/UserController.java
+++ backend/src/main/java/pw/bd2/SportTogether/controller/UserController.java
@@ -1,13 +1,35 @@
 package pw.bd2.SportTogether.controller;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pw.bd2.SportTogether.model.User;
 import pw.bd2.SportTogether.service.JwtService;
+import pw.bd2.SportTogether.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
 public class UserController {
-    private final JwtService jwtService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @GetMapping("/me")
+    public ResponseEntity<User> getCurrentUser(@RequestHeader(HttpHeaders.AUTHORIZATION) String jwt) {
+        try {
+            String username = jwtService.extractUsername(jwt);
+            User user = userService.findByUsername(username);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
 }
--- backend/src/main/java/pw/bd2/SportTogether/service/UserService.java
+++ backend/src/main/java/pw/bd2/SportTogether/service/UserService.java
@@ -1,8 +1,10 @@
 package pw.bd2.SportTogether.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pw.bd2.SportTogether.model.User;
 import pw.bd2.SportTogether.repository.UserRepository;
 
 @Service
@@ -10,4 +12,8 @@ import pw.bd2.SportTogether.repository.UserRepository;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElseThrow(() -> new EntityNotFoundException("User not found"));
+    }
 }
