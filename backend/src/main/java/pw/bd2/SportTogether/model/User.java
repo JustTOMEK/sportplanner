221c74d6fff56e4dc40a3b1d4d33f2111541ac63
dominika232323 <dominikaboguszewska62@gmail.com>
2024-06-23 22:30:58 +0200

    Add profile_picture to model

--- backend/src/main/java/pw/bd2/SportTogether/model/User.java
+++ backend/src/main/java/pw/bd2/SportTogether/model/User.java
@@ -29,6 +29,9 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    @Lob
+    private byte[] profile_picture;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(role.name()));
