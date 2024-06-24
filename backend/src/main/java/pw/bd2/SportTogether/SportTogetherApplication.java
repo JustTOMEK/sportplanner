c1884fb2dc84b7fb986e45b2b9683ec1ef583ede
Michał Machnikowski <01178559@pw.edu.pl>
2024-06-24 17:23:53 +0200

    Fixed event details page not working
    Dates were returned by backend for some reason in wrong format – as 5 numbers instead of one ISO string

--- backend/src/main/java/pw/bd2/SportTogether/SportTogetherApplication.java
+++ backend/src/main/java/pw/bd2/SportTogether/SportTogetherApplication.java
@@ -2,10 +2,9 @@ package pw.bd2.SportTogether;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @SpringBootApplication
-@EnableWebMvc
 public class SportTogetherApplication {
 
 	public static void main(String[] args) {
